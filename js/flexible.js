066a42dcde176b9e04ac979c2f7b8f50224cffdf
sangdi <1592807515@qq.com>
2022-01-31 19:12:16 +0800

    rge

--- js/flexible.js
+++ js/flexible.js
@@ -1,9 +1,9 @@
-(function flexible (window, document) {
+(function flexible(window, document) {
   var docEl = document.documentElement
   var dpr = window.devicePixelRatio || 1
 
   // adjust body font size
-  function setBodyFontSize () {
+  function setBodyFontSize() {
     if (document.body) {
       document.body.style.fontSize = (12 * dpr) + 'px'
     }
@@ -14,8 +14,8 @@
   setBodyFontSize();
 
   // set 1rem = viewWidth / 10
-  function setRemUnit () {
-    var rem = docEl.clientWidth / 24
+  function setRemUnit() {
+    var rem = docEl.clientWidth / 20
     docEl.style.fontSize = rem + 'px'
   }
 
